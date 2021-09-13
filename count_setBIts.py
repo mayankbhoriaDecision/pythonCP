7dc33449a37badebdfd4369b35be17395fee7eb6
mayankbhoriaDecision <80088527+mayankbhoriaDecision@users.noreply.github.com>
2021-09-14 01:03:28 +0530

    Create count_setBIts.py

---
+++ count_setBIts.py
@@ -0,0 +1,18 @@
+# Function to get no of set bits in binary
+# representation of passed binary no. */
+def countSetBits(n):
+
+	count = 0
+	while (n):
+		n &= (n-1)
+		count+= 1
+	
+	return count
+
+
+# Program to test function countSetBits
+i = 9
+print(countSetBits(i))
+
+# This code is contributed by
+# Smitha Dinesh Semwal
