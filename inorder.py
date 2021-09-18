ca3c8102cc84c800e02752a657aab46bf702d8e9
mayankbhoriaDecision <80088527+mayankbhoriaDecision@users.noreply.github.com>
2021-09-18 18:57:10 +0530

    Create inorder.py

---
+++ inorder.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        
+        """
+        
+        res=[]
+        
+        self.inorder(root,res)
+        
+        return res
+        
+        
+        
+    def inorder(self,root,res):
+     
+        if root is not None:
+            self.inorder( root.left,res)
+            
+            res.append((root.val))
+        
+            self.inorder( root.right,res)
