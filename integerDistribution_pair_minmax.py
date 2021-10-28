1e4864e662e11e0e1179ce069f2b00ca6bf3a1cf
mayankbhoriaDecision <80088527+mayankbhoriaDecision@users.noreply.github.com>
2021-10-28 18:54:15 +0530

    Create integerDistribution_pair_minmax.py

---
+++ integerDistribution_pair_minmax.py
@@ -0,0 +1,173 @@
+def FindIndex(a,N,X):
+
+
+    for i in range(N):
+
+        for j in range(N):
+
+            if(a[i][j]==X):
+
+                temp=[]
+                temp.append(i)
+                temp.append(j)
+
+                return  temp
+
+    return -1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def MinMaxPair(a):
+
+
+    N=len(a)
+
+
+    dp=[]
+
+    for i in range(N):
+
+        tmp=[]
+
+        for j in range(N):
+
+            tmp.append(0)
+
+        dp.append(tmp)
+
+
+
+
+
+
+
+
+    calc=[]
+
+
+    for i in range(N):
+        for j in range(N):
+
+
+
+            dp[i][j]=a[i]^a[j]
+
+            if(dp[i][j]!=0):
+
+                calc.append(dp[i][j])
+
+
+
+
+    calc.sort()
+
+
+    max_index=[]
+
+    max_sum=0
+
+    mcalc=calc
+
+    mcalc.reverse()
+
+
+    target=N/2
+
+
+   # print(mcalc)
+
+    for val in mcalc:
+
+        if(target>0):
+
+            temp=FindIndex(dp,N,val)
+
+            if(temp!=-1):
+
+                if(  temp[0] not in max_index and temp[1] not in max_index     ):
+
+                    max_index.append(temp[0])
+                    max_index.append(temp[1])
+
+                    max_sum+=val
+
+                    target-=1
+
+
+
+
+
+
+#minimum
+
+    min_sum=0
+
+    min_index=[]
+
+    target=N/2
+
+
+    calc.reverse()
+
+   # print(calc)
+
+    for val in calc:
+
+        if(target>0):
+
+            temp=FindIndex(dp,N,val)
+
+            if(temp!=-1):
+
+                if(  temp[0] not in min_index and temp[1] not in min_index     ):
+
+                    min_index.append(temp[0])
+                    min_index.append(temp[1])
+
+                    min_sum+=val
+
+                    target-=1
+
+
+    res=[]
+
+    res.append(min_sum)
+    res.append(max_sum)
+
+    return res 
+
+
+
+
+
+
+
+
+
+
+def Min_and_Max (n,Arr):
+    # // returns an array where the first element is the minimum sum and the
+    # second is the maximum sum .
+
+    res=MinMaxPair(Arr)
+
+    return (res)
