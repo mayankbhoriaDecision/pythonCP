5063ec68f1090cdd2ec05eda0fe16a2c6f3caf62
mayankbhoriaDecision <80088527+mayankbhoriaDecision@users.noreply.github.com>
2021-10-29 16:01:44 +0530

    Update maskCreator.py

--- maskCreator.py
+++ maskCreator.py
@@ -2,11 +2,19 @@ def subset(arr):
 
     N=len(arr)
     total_set=1<<N
+
+
+
     for mask in range(total_set):
+
+        tempSet=[]
 
         for i in range(N):
 
             f=1<<i
 
             if(mask&f)!=0:
-                print(bin(mask)[2:])
+
+                tempSet.append(arr[i])
+
+        print(tempSet)
