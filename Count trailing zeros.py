4ef13347ff39e60abc12c17f054943bf546cbd4e
mayankbhoriaDecision <80088527+mayankbhoriaDecision@users.noreply.github.com>
2022-07-28 00:15:38 +0530

    Create Count trailing zeros.py

---
+++ Count trailing zeros.py
@@ -0,0 +1,19 @@
+
+def Count_Trailing_zeroes_in_factorial(N):
+    
+    res=0
+    
+    i=5
+    
+    while(i<=N):
+        
+        res=res+N//i 
+        N=N//i
+        i=i*5
+        
+    return res 
+
+Num=int(input())
+
+
+print(Count_Trailing_zeroes_in_factorial(Num))
